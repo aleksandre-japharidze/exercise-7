590010c7335a53cbeb1033d2ab647f99451146f0
AleksandreJapharidze <alexanderjapharidze@gmail.com>
2025-11-27 00:53:28 +0400

    Added customer

--- python/program.py
+++ python/program.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from tkinter import N
+from dataclasses import dataclass
 
 class Product(ABC):
     def __init__(self, name, base_price):
@@ -30,14 +30,21 @@ class Electronics(Product):
 products = []
 
 class ShoppingCart:
-    def __init__(self, products=None):
+    def __init__(self, products=None, customer=None):
         self.__products = products if products is not None else []
+        self.__customer = customer if customer is not None else ""
 
     def get_products(self):
         return self.__products
 
     def set_products(self, products):
         self.__products = products
+
+    def get_customer(self):
+        return self.__customer
+
+    def set_customer(self, customer):
+        self.__customer = customer
 
     def add_product(self, product):
         self.__products.append(product)
@@ -74,10 +81,17 @@ sc.remove_product("Apple")
 print(sc.get_products())
 print("\n")
 
-another_sc = ShoppingCart()
+@dataclass
+class Person:
+    id: int
+    name: str
+    has_premium: bool
+
+another_sc = ShoppingCart(customer=Person(1, "John", True))
 sc.move_to_another_cart(another_sc)
 print(another_sc.get_products())
 print(sc.get_products())
 
 another_sc.add_product(Food("Apple", 100))
 print(another_sc.sum_prices())
+print(another_sc.get_customer())
